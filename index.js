6c89315ed198743baf196abbe81d04c0bc66f00b
Augustin Hannebert <augustin@hannebert.com>
2023-03-15 10:46:38 +0100

    ajout des cors 4

--- index.js
+++ index.js
@@ -21,12 +21,12 @@ app.get("/formulaire", function(req, res) {
 });
 
 //problème des cors résolu ...?
-// app.use((req, res, next) => {
-//     res.header('Access-Control-Allow-Origin', '*');
-//     res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
-//     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-//     next();
-// });
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+    next();
+});
 
 //crée les annotations dans un objet dico json
 app.post("/formulaire", cors(),function(req, res){
